8dcfe71c09c7de73320c911c2cf7576a23b9d2e6
Krish Suchak <suchak.krish@gmail.com>
2019-10-09 22:20:38 -0500

    get_name and set_name

--- test/test_community.cpp
+++ test/test_community.cpp
@@ -7,7 +7,7 @@ class test_community: public ::testing::Test {
 protected:
 	Community community;
 	Community* loaded = new Community("Summerbrooke", map<string,Person>
-		{{"null", Person()}});
+		{{"alpha1", Person("alpha1", "Ash", "Ketchum", 2, 18, "Pikachu")}});
 };
 
 // test constructors
@@ -17,8 +17,8 @@ TEST_F(test_community, test_constructors) {
 
 	EXPECT_EQ(loaded -> get_name(), "Summerbrooke");
 	EXPECT_EQ(loaded -> get_all_usernames().empty(), false);
-	EXPECT_EQ(loaded -> get_all_usernames().front(), "null");
-	EXPECT_EQ(loaded -> get_member(loaded -> get_all_usernames().front()).get_username(), "");
+	EXPECT_EQ(loaded -> get_all_usernames().front(), "alpha1");
+	EXPECT_EQ(loaded -> get_member(loaded -> get_all_usernames().front()).get_username(), "alpha1");
 }
 
 // you should complete the following test cases
@@ -26,10 +26,18 @@ TEST_F(test_community, test_constructors) {
 
 // test get_name
 TEST_F(test_community, get_name) {
+	EXPECT_EQ(community.get_name(), "");
+	EXPECT_EQ(loaded -> get_name(), "Summerbrooke");
 }
 
 // test set_name
 TEST_F(test_community, set_name) {
+	EXPECT_EQ(community.set_name(""), false);
+	EXPECT_EQ(community.set_name(std::string (129, 'x')), false);
+	EXPECT_EQ(community.set_name("1alpha"), false);
+	EXPECT_EQ(community.set_name("alpha_"), false);
+	EXPECT_EQ(community.set_name("alpha"), true);
+	EXPECT_EQ(community.set_name("alpha1"), true);
 }
 
 // test add_person
